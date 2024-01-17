f1fe91f4ffe3af4071f70a4cc4774c9d62bb6fbc
Grzegorz Malisz <g.malisz@student.fontys.nl>
2024-01-17 15:30:00 +0100

    clean up

--- mdp.py
+++ mdp.py
@@ -3,27 +3,12 @@ from math import isclose
 import numpy as np
 
 class Mdp:
-    def __init__(self, states, actions, transition_probabilities, rewards, gamma=0.9,
-                 eps=1e6, random_termination=0.0, cost_of_living=0.0):
+    def __init__(self, states, actions, transition_probabilities, rewards):
         self.states = states
         self.actions = actions
         self.transition_probabilities = transition_probabilities
         self.inspect_probabilities()
         self.rewards = rewards
-
-        self.gamma = gamma
-        self.eps = eps
-        self.random_termination = random_termination
-        assert 0 <= self.random_termination <= 1
-        self.cost_of_living = cost_of_living
-
-        # self.value = {}
-        # for state in states:
-        #     self.value[state] = 0.0
-
-    # def reset(self):
-    #     for state in self.states:
-    #         self.value[state] = 0.0
 
     def lookup_transition_probability(self, state, action, next_state):
         return self.transition_probabilities.get(state, {}).get(action, {}).get(next_state, 0.0)
@@ -50,25 +35,3 @@ class Mdp:
         reward = self.lookup_reward(current_state, action, new_state)
         is_terminal = len(self.possible_actions_indexes(new_state)) == 0
         return (new_state, reward, is_terminal)
-
-    # def value(self, state):
-    #     pass
-
-    # def action_value(self, state, action):
-    #     next_states = self.transition_probabilities[state].get(action, {})
-    #     return sum(self.lookup_transition_probability(state, action, next_state) * (
-    #             self.lookup_reward(state, action, next_state) + self.gamma * self.value[next_state]) for next_state
-    #                in next_states)
-
-    # def state_value(self, state, action):
-    #     next_states = self.transition_probabilities[state].get(action, {})
-    #     return sum(self.lookup_transition_probability(state, action, next_state) * (
-    #             self.lookup_reward(state, action, next_state) + self.gamma * self.value[next_state]) for next_state
-    #                in next_states)
-
-    # return random.choice(self.actions)
-
-    # def estimate_value(self):
-    #     for _ in range(int(self.eps)):
-    #         for state in self.states:
-    #             self.value[state] = self.action_value(state, self.policy(state))
